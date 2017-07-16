ae8f22bffdb445d281eb30057e3735620f703c63
Hanh Vo <hanh.vo.programmer@gmail.com>
2017-07-16 18:47:36 +0700

    Add Minimum Absolute Difference in an Array

---
+++ 00.Algorithm/06.MinimumAbsoluteDifferenceinanArray.py
@@ -0,0 +1,20 @@
+import sys
+
+
+# n = int(input().strip())
+# a = list(map(int, input().strip().split(' ')))
+# # your code goes here
+
+n = 3
+a = [3, -7, 0]
+
+a.sort()
+i = 1
+min_absolute = abs(a[0] - a [1])
+while i < n - 1:
+    min_absolute = min(min_absolute, abs(a[i] - a[i + 1]))
+    i += 1
+    
+    
+
+print (min_absolute)
