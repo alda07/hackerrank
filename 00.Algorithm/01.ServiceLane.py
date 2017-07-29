ca6608b1826a089057aab2b1e3d1128f841fff92
Hanh Vo <hanh.vo.programmer@gmail.com>
2017-07-29 22:49:06 +0700

    Add Climbing the Leader board

--- 00.Algorithm/01.ServiceLane.py
+++ 00.Algorithm/01.ServiceLane.py
@@ -1,14 +1,4 @@
-# import sys
-
-
-# n,t = raw_input().strip().split(' ')
-# n,t = [int(n),int(t)]
-# width = map(int,raw_input().strip().split(' '))
-# for a0 in xrange(t):
-#     i,j = raw_input().strip().split(' ')
-#     i,j = [int(i),int(j)]
-
-
+#input 
 # 8 5
 # 2 3 1 2 3 2 3 3
 # 0 3
@@ -16,15 +6,29 @@
 # 6 7
 # 3 5
 # 0 7
+#output
+# 1
+# 2
+# 3
+# 2
+# 1
+
+import sys
 
 def find_numer_of_vihicle(from_index,to_index,arr_width):
     i = from_index
     min_vihicles = arr_width[from_index]
     while i <= to_index:
         min_vihicles = min(min_vihicles,arr_width[i])
-        print (min_vihicles)
         i += 1
     return min_vihicles
 
-width = [2, 3, 1 ,2 ,3 ,2 ,3, 3]
-print (find_numer_of_vihicle(0,3,width))
+
+n,t = raw_input().strip().split(' ')
+n,t = [int(n),int(t)]
+width = map(int,raw_input().strip().split(' '))
+for a0 in xrange(t):
+    i,j = raw_input().strip().split(' ')
+    i,j = [int(i),int(j)]
+    print (find_numer_of_vihicle(i,j,width))
+    
