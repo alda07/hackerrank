94bd093c1a5d71e99dd214700e91e28402182942
Hanh Vo <hanh.vo.programmer@gmail.com>
2017-08-30 13:55:44 +0700

    Add Set Intersection.

--- 03.Python/03.Set_Intersection.py
+++ 03.Python/03.Set_Intersection.py
@@ -5,20 +5,13 @@
 # 10 1 2 3 11 21 55 6 8
 
 # ouput
-# 13
+# 5
+
 n_english_subscribes = (int)(input())
 english_subscribes = set(map(int, input().split()))
 
 n_french_subscribes = (int)(input())
 french_subscribes = set(map(int, input().split()))
 
-union_all = english_subscribes | french_subscribes 
-# or
-# union_all = english_subscribes.union(french_subscribes)
-# is the same
-
-
-print(len(union_all))
-
-
-
+intersections = english_subscribes.intersection(french_subscribes)
+print (len(intersections))
