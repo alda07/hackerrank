6794a0e77cf1c8d60bc8c933648d5428f1563c04
Hanh Vo <hanh.vo.programmer@gmail.com>
2017-10-02 11:32:21 +0700

    Add Validating_phone_numbers

--- 03.Python/12.Validating_Roman_Numerals.py
+++ 03.Python/12.Validating_Roman_Numerals.py
@@ -4,21 +4,11 @@
 # Sometimes , I feel so stupid.
 
 
-from roman import fromroman
+import re
 
-try:
-    if 0<fromRoman(input())<4000:
-        print(True)
-    else:
-        print(False)
-except:
-    print(False)
-
-# import re
-
-# s = input()
-# isRomanNumeral = True
-# patern = '^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$'
-# m = re.search(r'%s' % patern, s)
-# print (bool(m))
+s = input()
+isRomanNumeral = True
+patern = '^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$'
+m = re.search(r'%s' % patern, s)
+print (bool(m))
         
