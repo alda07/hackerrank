79e70c3168ed464560c46ca9c3f7bbff8dfdf9eb
Hanh Vo <hanh.vo.programmer@gmail.com>
2017-11-30 11:13:23 +0700

    Add ArrayMathematics

--- 03.Python/15.EyeAndIdentity.py
+++ 03.Python/15.EyeAndIdentity.py
@@ -2,13 +2,16 @@
 
 # print(numpy.identity(3))
 
-import numpy
+#import numpy
 # print(numpy.eye(8, 8, k = 1))
 # print ("-------------------------")
 # print(numpy.eye(8, 8, k = 0))
 # print ("-------------------------")
 # print ("-------------------------")
 # print (numpy.eye(8, 8, k = -1))
+
+
+import numpy
 
 n, m = input().split()
 n, m = int(n), int(m)
