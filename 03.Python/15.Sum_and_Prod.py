51397b43e1c312b390e44322246b000e4f10756f
Hanh Vo <hanh.vo.programmer@gmail.com>
2017-12-01 14:46:31 +0700

    Add Min_and_Max

--- 03.Python/15.Sum_and_Prod.py
+++ 03.Python/15.Sum_and_Prod.py
@@ -1,25 +1,8 @@
 # sum
 # returns the sum of array elements over a given axis
 
-# import numpy
-
-# my_array = numpy.array([[1, 2, 3], [3, 4, 5], [6,7,8]])
-
-# print (numpy.sum(my_array, axis = 0))
-# print (numpy.sum(my_array, axis = 1))
-# print (numpy.sum(my_array)
-# )
-
 # prod
 # return product of array elements over a given axis -> product element A * element B
-
-# import numpy
-
-# my_array = numpy.array([[1, 2], [3, 4]])
-
-# print (numpy.prod(my_array, axis = 0))
-# print (numpy.prod(my_array, axis = 1))
-# print (numpy.prod(my_array, axis = None))
 
 import numpy
 
@@ -31,4 +14,4 @@ for i in range(n):
     
 sum_array = numpy.sum(array, axis = 0)
 product_array = numpy.prod(sum_array)
-print (sum_array, product_array, sep = '\n')
+print (product_array)
