2e2fe8cd3de1e914b441bb5290ebd5bedb5750f3
Hanh Vo <hanh.vo.programmer@gmail.com>
2017-12-28 14:46:39 +0700

    Add 01.Non-DivisibleSubset

---
+++ 00.Algorithm/01.Non-DivisibleSubset.py
@@ -0,0 +1,26 @@
+n,k = input().strip().split()
+n, k = int(n), int(k)
+
+a = list(map(int, input().split()))
+
+L = [0]*k
+
+for x in a: 
+    L[x % k] += 1
+res = 0
+for i in range(k//2+1):
+    if i == 0 or k == i*2:
+        res += bool(L[i])
+    else:
+        res += max(L[i], L[k-i])
+
+print(res)
+
+# 4 3
+# 1 7 2 4
+
+
+# 6 9
+# 422346306 940894801 696810740 862741861 85835055 313720373
+
+# 5
